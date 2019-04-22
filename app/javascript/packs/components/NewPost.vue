bfca1a0be61e05d4f35f826e2e7627a95bea6706
Linh Nguyen <linhvn09@gmail.com>
2019-04-22 13:05:55 +0700

    [frontend] restyle new post form

--- app/javascript/packs/components/NewPost.vue
+++ app/javascript/packs/components/NewPost.vue
@@ -1,9 +1,9 @@
 <template>
   <b-container fluid>
-    <b-form>
-      <b-form-row md="12"><b-col cols="12"><b-input-group><b-form-input placeholder="Post title" /></b-input-group></b-col></b-form-row>
-      <b-form-row md="12"><b-col cols="12"><vue-editor v-model="content" /></b-col></b-form-row>
-      <b-form-row md="12"><b-col cols="12"><b-button type="submit" variant="primary">Save</b-button></b-col></b-form-row>
+    <b-form class="mw-100">
+      <b-form-row md="12" sm="12" xs="10" class="mb-2"><b-col cols="12"><b-input-group><b-form-input placeholder="Post title" /></b-input-group></b-col></b-form-row>
+      <b-form-row md="12" sm="12" xs="10" class="mb-2"><b-col cols="12"><vue-editor v-model="content" /></b-col></b-form-row>
+      <b-form-row md="12" sm="12" xs="10" class="mb-2"><b-col cols="12"><b-button type="submit" variant="primary">Save</b-button></b-col></b-form-row>
     </b-form>
   </b-container>
   
