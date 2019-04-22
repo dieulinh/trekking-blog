e230bb1023fdb72e06e51deef3db8c78f29dd4e9
Linh Nguyen <linhvn09@gmail.com>
2019-04-22 16:47:56 +0700

    show list post from server

--- app/javascript/packs/components/Posts.vue
+++ app/javascript/packs/components/Posts.vue
@@ -11,7 +11,7 @@
 </template>
 <script>
 import axios from '../common/axios';
-const postApiUrl = 'http://localhost:3000/api/v1/posts';
+const postApiUrl = 'https://trekking-blog.herokuapp.com/api/v1/posts';
 export default {
   data() {
     return { posts: []};
