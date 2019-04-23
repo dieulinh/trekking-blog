af0220aa05b8913211cb4c7af3c5896d7d1024a2
Linh Nguyen <linhvn09@gmail.com>
2019-04-23 10:43:43 +0700

    [frontend] update new post component

--- app/javascript/packs/components/NewPost.vue
+++ app/javascript/packs/components/NewPost.vue
@@ -2,6 +2,7 @@
   <b-container fluid>
     <b-form class="mw-100">
       <b-form-row md="12" sm="12" xs="10" class="mb-2"><b-col cols="12"><b-input-group><b-form-input placeholder="Post title" v-model="title"/></b-input-group></b-col></b-form-row>
+      <b-form-row md="12" sm="12" xs="10" class="mb-2"><b-col cols="12"><b-input-group><b-form-input placeholder="Post description" v-model="description"/></b-input-group></b-col></b-form-row>
       <b-form-row md="12" sm="12" xs="10" class="mb-2"><b-col cols="12"><vue-editor v-model="content" /></b-col></b-form-row>
       <b-form-row md="12" sm="12" xs="10" class="mb-2"><b-col cols="12"><b-button @click="handlePost" variant="primary">Save</b-button></b-col></b-form-row>
     </b-form>
@@ -25,7 +26,8 @@ export default {
   data() {
     return {
       title: '',
-      content: 'Some initial content'
+      content: 'Some initial content',
+      description: ''
     }
   },
   mounted() {
@@ -33,7 +35,7 @@ export default {
   },
   methods: {
     handlePost() {
-      axios.post(postApiUrl, {title: this.title, content: sanitizeHtml(this.content)}).then((response) => {
+      axios.post(postApiUrl, { title: this.title, description: this.description, user_id: 1, content: sanitizeHtml(this.content)}).then((response) => {
       }).catch((err) => {
         console.log(`Error: ${err}`);
       });
