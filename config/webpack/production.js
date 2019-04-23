510bd6cbd1eeaf4cbad998583b3aa98abb8f52e8
Linh Nguyen <linhvn09@gmail.com>
2019-04-23 22:02:15 +0700

    [frontend] fix api path

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-process.env.ROOT_API='"https://trekking-blog.herokuapp.com/api/v1"'
+process.env.ROOT_API='https://trekking-blog.herokuapp.com/api/v1'
 
 const environment = require('./environment')
 
