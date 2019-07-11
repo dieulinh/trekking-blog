6b0fb186809bc614043983dacb43ac8f99b26738
Linh Nguyen <linhvn09@gmail.com>
2019-07-11 19:16:03 +0700

    fix links

--- app/javascript/packs/components/Welcome.vue
+++ app/javascript/packs/components/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div class="card-body text-center" >
-      <p>{{ message }}</p>
+      <p v-cloak="">{{ message }}</p>
       <div class="container"> 
         <a href="http://www.eagertrekker.com/#/posts/yume-no-turibashi-the-dreaming-bridge">Japan</a>
         <img src="http://trekking-app.s3-ap-southeast-1.amazonaws.com/10603651_1714158508808583_1919393890660913503_n.jpg"/>
