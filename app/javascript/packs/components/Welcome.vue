5525675b15418df313ecc6ca4b8f0d5e92ba34b0
Linh Nguyen <linhvn09@gmail.com>
2019-08-06 16:08:17 +0700

    change logo

--- app/javascript/packs/components/Welcome.vue
+++ app/javascript/packs/components/Welcome.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
     <div class="container">
-      <h2>
+      <h5>
         Latest Posts
-      </h2>
+      </h5>
       <hr/>
     </div>
     <div class="container sp_sidebar">
