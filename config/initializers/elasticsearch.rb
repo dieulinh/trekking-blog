d7f9fa09abe780327ad363f3115ddc5c3a0cd23d
Linh Nguyen <linhvn09@gmail.com>
2019-08-06 12:12:10 +0700

    fix build run error

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -6,8 +6,8 @@ config = {
   logger: Logger.new(STDERR)
 }
 
-if File.exists?("config/elasticsearch.yml")
-  config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
-end
+# if File.exists?("config/elasticsearch.yml")
+#   config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
+# end
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
