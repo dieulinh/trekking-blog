8bc9a124aac8b179d50c626ba3c6aca27e2decf2
Linh Nguyen <linhvn09@gmail.com>
2019-08-10 22:47:34 +0700

    refactor code of post repository

--- app/elasticsearch_repository/application_repository.rb
+++ app/elasticsearch_repository/application_repository.rb
@@ -5,7 +5,7 @@
 class ApplicationRepository < Elasticsearch::Persistence::Repository::Class
   def initialize(options = {})
     @options = options
-    client Elasticsearch::Client.new url: "http://ec2-54-255-208-1.ap-southeast-1.compute.amazonaws.com:9200", log: true
+    client Elasticsearch::Client.new url: ENV['ELASTICSEARCH_HOST'], log: true
 
     index_name [Rails.application.engine_name, Rails.env, self.class.name].map(&:downcase).join('_').gsub!('::', '__')
     type [Rails.application.engine_name, Rails.env, self.class.name].map(&:downcase).join('_').gsub!('::', '__')
@@ -13,7 +13,6 @@ class ApplicationRepository < Elasticsearch::Persistence::Repository::Class
 
     setting_repository
     block.arity < 1 ? instance_eval(&block) : yield(self) if block_given?
-
   end
 
 
