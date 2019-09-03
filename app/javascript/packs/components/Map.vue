6c13aa3f73a68cde74fff7a1e836f5ad3a76d4c0
Dieu Linh Nguyen <linhvn09@gmail.com>
2019-09-03 10:56:32 +0000

    add cf key to map

--- app/javascript/packs/components/Map.vue
+++ app/javascript/packs/components/Map.vue
@@ -22,7 +22,7 @@ import Vue from 'vue';
 import * as VueGoogleMaps from 'vue2-google-maps';
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.GOOGLE_MAP_API_KEY,
+    key: 'AIzaSyB87QOBbL18pW8vfnFFakEgWTBR5yvPJns',
     libraries: 'places', // This is required if you use the Autocomplete plugin
     // OR: libraries: 'places,drawing'
     // OR: libraries: 'places,drawing,visualization'
