043fde3d04e764334cb4f164d336a91107f7c3f2
Dieu Linh Nguyen <linhvn09@gmail.com>
2019-09-03 09:37:25 +0000

    add google map cf

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 process.env.ROOT_API='https://trekking-blog.herokuapp.com/api/v1'
-process.env.GOOGLE_MAP_API_KEY = 'AIzaSyDdB12teSjaIYUIRQAqAxVU3iJfaYfDD-o'
+process.env.GOOGLE_MAP_API_KEY='AIzaSyB87QOBbL18pW8vfnFFakEgWTBR5yvPJns'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
