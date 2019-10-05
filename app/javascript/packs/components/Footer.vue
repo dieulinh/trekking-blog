7a6427068818c3a5413c4722e92a48fb0f3c2eb8
Linh Nguyen <linhvn09@gmail.com>
2019-10-05 21:37:29 +0700

    change text footer

--- app/javascript/packs/components/Footer.vue
+++ app/javascript/packs/components/Footer.vue
@@ -4,25 +4,10 @@
           <div class="widget-content">
             <center>
               <div class="about-widget">
-                <h5>About me</h5>
-                <p>I am Linh Nguyen, a freelance Full-stack web developer.</p>
                 <p>
-                  5+ years of experience.
-
-                  Experienced working with team
-                  or working independently.
-
-
-                  MY SKILLS
-
-                  - Ruby on Rails ( ♥ )
-                  - Javascript (Angular, Vue, React)
-                  - SQL (MySQL, Postgres)
-                  - CSS (SASS/LESS)
-                  - MongoDB, ElasticSearch
-                  - Redis
-                  - AWS
-              </p>
+                  A place to test everything about web development
+                </p>
+                <strong>IG: @linhavidtraveler </strong>
               </div>
             </center>
           </div>
