2ebb9bf42af4aebf9e99c7aa44cb9d217c6b82f2
Linh Nguyen <linhvn09@gmail.com>
2019-10-19 12:01:05 +0700

    implement show search post result

--- app/javascript/packs/components/Posts.vue
+++ app/javascript/packs/components/Posts.vue
@@ -1,5 +1,14 @@
 <template>
   <b-container>
+    <div class="app-header">
+      <div class="relative-search">
+        <label for="search_query" class="sr-only">Search for:</label>
+        <input placeholder="Search for..." v-model='terms' name="search_query" type="text" v-on:keyup.enter="getPosts(page)" class="form-control input-focus all-animate w-100 bg-white db shadow-inset-2 ba bw1 br2 pv3 pl4 pr6 lh-solid sans-serif b--gray3 fw6 f5 gray7"> <!----> 
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="o-15 absolute center-v right-1 pe-none svg-inline--fa fa-search fa-w-16 fa-fw fa-lg">
+        <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" class=""></path></svg> 
+      </div>
+    </div>
+
     <div v-if="authenticated" class="row mt-5">
       <div class="col-md-12 mb-5 float-left">
         <router-link class="btn btn-primary" :to="{ name: 'NewPost'}">
@@ -74,6 +83,7 @@ export default {
     return { 
       posts: [],
       page: 0,
+      terms: null,
       pages: [],
       totalPages: 0,
       authenticated: false
@@ -88,7 +98,7 @@ export default {
   mounted(){
     axios.get(`${postApiUrl}?page=${this.page+1}`)
     .then((response) => {
-      console.log(response.data.posts);
+      
       this.posts = response.data.posts;
       this.totalPages = response.data.total_pages;
       this.pages = [...Array(response.data.total_pages).keys()]
@@ -107,14 +117,20 @@ export default {
   },
   methods: {
     getPosts(page) {
-      axios.get(`${postApiUrl}?page=${page+1}`).then((response) => {
-      this.posts = response.data.posts;
-      this.page = page;
-      this.pages = [...Array(response.data.total_pages).keys()];
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      let pageParams = `page=${page+1}`;
+      if (this.terms) {
+        pageParams = `terms=${this.terms}&page=${page+1}`;
+      }
+      
+      axios.get(`${postApiUrl}?${pageParams}`).then((response) => {
+        console.log(response.data);
+        this.posts = response.data.posts;
+        this.page = page;
+        this.pages = [...Array(response.data.total_pages).keys()];
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     }
   }
 }
